b588115b4f0788248cf73be920c9b2ec27d88306
mengo <menghong6988@gmail.com>
2025-09-22 00:58:08 +0900

    feat: add wagmi and abi

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,15 +3,19 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { WalletProvider } from "@/contexts/WalletContext";
+import { WagmiProvider } from "wagmi";
+import { config } from "@/config/config";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <WalletProvider>
-        <Component {...pageProps} />
-      </WalletProvider>
-    </QueryClientProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <WalletProvider>
+          <Component {...pageProps} />
+        </WalletProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
