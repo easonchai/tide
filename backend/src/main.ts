d2e1aa98abc7d44fd6ed1c7a59a2044ff94438a7
Eason Chai <echai2905@gmail.com>
2025-09-22 14:43:11 +0900

    feat: Add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://tidehl.vercel.app',
+    ],
     credentials: true,
   });
 
